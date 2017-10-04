7f942d8268b5516ed1cef8a3f0e0ba97ea0bed4d
CHIN Yen-Ting <ytchin@cw09.cc.kek.jp>
2017-10-05 00:36:54 +0900

    add skim without PID cuts

--- B+2mu+nu_mu.py
+++ B+2mu+nu_mu.py
@@ -1,18 +1,42 @@
 from basf2 import *
 from modularAnalysis import *
+from stdCharged import *
+from stdPi0s import *
+from stdV0s import *
+from stdCharm import *
 
 # input mdst file
 inputMdst('default','~/B+2mu+nu_mu/mdst/B+2mu+nu_mu_mdst.root')
-fillParticleList('mu+:all','')
-reconstructDecay('B+:all -> mu+:all','')
-matchMCTruth('B+:all')
-toolsmu = ['EventMetaData','^B+ -> ^mu+']
-toolsmu += ['MCTruth','^B+ -> ^mu+']
-toolsmu += ['Kinematics','^B+ -> ^mu+']
-toolsmu += ['MCKinematics','^B+ -> ^mu+']
-toolsmu += ['DeltaEMbc','^B+ -> mu+']
-ntupleFile('~/B+2mu+nu_mu/20171004_B+2mu+nu_mu2_recons.root')
-ntupleTree('muon','B+:all',toolsmu)
+
+# load particles
+loadStdCharged()
+loadStdPi0()
+loadStdKS()
+loadStdD0()
+loadStdDplus()
+loadStdDstar0()
+loadStdDstarPlus()
+
+# skim
+cutAndCopyList('mu-:highP','mu-:all','p>2.0',True)
+reconstructDecay('B+:L -> mu+:highP','')
+
+# reconstrucion
+
+
+# MC truth matching
+matchMCTruth('B+:L')
+
+# NTuple maker
+toolsmu = ['EventMetaData','^B- -> ^mu-']
+toolsmu += ['MCTruth','^B- -> ^mu-']
+toolsmu += ['Kinematics','^B- -> ^mu-']
+toolsmu += ['MCKinematics','^B- -> ^mu-']
+toolsmu += ['DeltaEMbc','^B- -> mu-']
+ntupleFile('~/B+2mu+nu_mu/20171004_B+2mu+nu_mu2_2_skim.root')
+ntupleTree('muon','B-:L',toolsmu)
+
+#processing
 process(analysis_main)
 
 
