ae50b25b7727a6915277695fa91ba5ac816e7cce
CHIN Yen-Ting <ytchin@cw03.cc.kek.jp>
2017-10-04 23:53:03 +0900

    add reconstruction

--- B+2mu+nu_mu.py
+++ B+2mu+nu_mu.py
@@ -3,14 +3,20 @@ from modularAnalysis import *
 
 # input mdst file
 inputMdst('default','~/B+2mu+nu_mu/mdst/B+2mu+nu_mu_mdst.root')
-
 fillParticleList('mu+:all','')
-
-matchMCTruth('mu+:all')
-toolsmu = ['EventMetaData','^mu+']
-toolsmu += ['MCTruth','^mu+']
-ntupleFile('B+2mu+nu_mu.root')
-ntupleTree('muon','mu+:all',toolsmu)
+reconstructDecay('B+:all -> mu+:all','')
+matchMCTruth('B+:all')
+toolsmu = ['EventMetaData','^B+ -> ^mu+']
+toolsmu += ['MCTruth','^B+ -> ^mu+']
+toolsmu += ['Kinematics','^B+ -> ^mu+']
+toolsmu += ['MCKinematics','^B+ -> ^mu+']
+toolsmu += ['DeltaEMbc','^B+ -> mu+']
+ntupleFile('~/B+2mu+nu_mu/20171004_B+2mu+nu_mu2_recons.root')
+ntupleTree('muon','B+:all',toolsmu)
 process(analysis_main)
+
+
+
+
 #print out the summary
 print(statistics)
